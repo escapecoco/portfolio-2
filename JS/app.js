b404b91cafc93287f0ad2cbc65fee433294aaad8
escapecoco <saad.hamdi.coco@gmail.com>
2023-01-04 20:17:30 +0300

    debug JS

--- JS/app.js
+++ JS/app.js
@@ -1,24 +1,3 @@
-/*
-const text = document.querySelector(".sec-text");
-
-const textLoad = () => {
-    setTimeout(() => {
-        text.textContent = "Freelancer";
-    }, 0);
-    setTimeout(() => {
-        text.textContent = "Blogger";
-    }, 4000);
-    setTimeout(() => {
-        text.textContent = "YouTuber";
-    }, 8000); //1s = 1000 milliseconds
-}
-
-textLoad();
-setInterval(textLoad, 12000);
-*/
-
-
-
 window.addEventListener('load', () => {
 
     setTimeout(() => {
